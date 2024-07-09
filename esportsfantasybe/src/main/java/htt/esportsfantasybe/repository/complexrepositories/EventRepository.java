13645993786bebf3f4abe2ffb85766ee3afe598e
Alberto Plaza <albertoplazamontesdm@gmail.com>
2024-07-09 19:05:29 +0200

    refactor: bad identation

--- esportsfantasybe/src/main/java/htt/esportsfantasybe/repository/complexrepositories/EventRepository.java
+++ esportsfantasybe/src/main/java/htt/esportsfantasybe/repository/complexrepositories/EventRepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, String>{
-    List<Event> findAllById_Realleagueuuid(UUID realleagueuuid)
-            ;
+    List<Event> findAllById_Realleagueuuid(UUID realleagueuuid);
     @Query("SELECT MAX(e.id.jour) FROM Event e WHERE e.id.realleagueuuid = :realLeagueUuid")
     int findMaxJour(@Param("realLeagueUuid") UUID realleagueuuid);
 }
