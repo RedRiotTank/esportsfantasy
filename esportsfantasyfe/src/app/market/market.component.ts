6c8ebffc0aeb58a6906d5e555811b29007c81024
Alberto Plaza <albertoplazamontesdm@gmail.com>
2024-07-17 00:02:55 +0200

    add: Role icons in front

--- esportsfantasyfe/src/app/market/market.component.ts
+++ esportsfantasyfe/src/app/market/market.component.ts
@@ -5,48 +5,44 @@ import { MarketService } from './market.service';
 import { MatDialog } from '@angular/material/dialog';
 import { BidupModalComponent } from './bidup-modal/bidup-modal.component';
 
-
 @Component({
   selector: 'app-market',
   templateUrl: './market.component.html',
-  styleUrls: ['./market.component.scss'] // Aquí está el cambio
+  styleUrls: ['./market.component.scss'], // Aquí está el cambio
 })
-export class MarketComponent implements OnInit{
-
-  
-
-  constructor(
-    private marketService: MarketService,
-    public dialog: MatDialog
-  ) { }
+export class MarketComponent implements OnInit {
+  constructor(private marketService: MarketService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.marketService.loadMarket();
   }
 
-  getMarketPlayers(){
+  getMarketPlayers() {
     return this.marketService.getMarketPlayers();
   }
 
   openModal(player: any): void {
     //console.log(player);
-    
+
     const dialogRef = this.dialog.open(BidupModalComponent, {
       width: '400px',
-      data: { name: player.name, uuid: player.uuid, price: player.price}
+      data: { name: player.name, uuid: player.uuid, price: player.price },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      
-      if(result){
-        this.marketService.bidUp(player.uuid, result.number).subscribe( () => {
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.marketService.bidUp(player.uuid, result.number).subscribe(() => {
           //console.log("Bid up success");
-          
         });
-        
       }
-
     });
   }
 
+  checkImageExists(player: any): boolean {
+    const imgPath = `assets/${player.role}.webp`;
+    const http = new XMLHttpRequest();
+    http.open('HEAD', imgPath, false);
+    http.send();
+    return http.status !== 404;
+  }
 }
